401ccc996045ba567be253d05bec8237731ea818
mrayandika15 <mrayandika15@gmail.com>
2023-05-16 14:21:53 +0700

    BREAKING CHANGES : reinstall next js

--- src/components/DIvider/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import style from "./Divider.module.css";
-
-const Divider = () => {
-  return <div className={style.divider} />;
-};
-
-export default Divider;
--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import Header from "./Header";
-import Button from "./Button";
-import Divider from "./DIvider";
-
-export { Header, Button, Divider };
